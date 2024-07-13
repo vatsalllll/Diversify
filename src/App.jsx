6741a4b8c46b2eead9488e15469ae7bfe9f0308b
amlanxbghn <amlanxconnect@gmail.com>
2024-07-13 19:27:46 +0530

    Navbar and Hero Section

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navbar from './components/Navbar/Navbar';
+import Main from './components/Main/Main';
+import Features from './components/Features/Features';
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <Main />
+      <Features/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Main.css';
+
+const Main = () => {
+    return (
+        <div className="main">
+            <div className='hero'>
+                <h1>Cross-Platform <span className='highlight'>Content</span> Diversification Tool</h1>
+                <h3>Bridge Platforms Seamlessly. Empower Your Presence Everywhere.</h3>
+                <div className="buttons">
+                    <button className='convert'>Convert</button>
+                    <h5>or</h5>
+                    <button className='secbutton'>Start Writting</button>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Main;
