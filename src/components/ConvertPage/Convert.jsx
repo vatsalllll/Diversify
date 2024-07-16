7b7365fc489d4b3000d712374aa067b2c1a380a6
amlanxbghn <amlanxconnect@gmail.com>
2024-07-16 23:35:43 +0530

    Convert Page

--- src/components/ConvertPage/Convert.jsx
+++ src/components/ConvertPage/Convert.jsx
@@ -4,21 +4,27 @@ import './Convert.css';
 const Convert = () => {
     return (
         <div className="selectMenu">
-            <div className="convertSection">
-                <h1>Convert From</h1>
-                <select className="dropdown">
-                    <option value="option1">Option 1</option>
-                    <option value="option2">Option 2</option>
-                    <option value="option3">Option 3</option>
-                </select>
+            <div className="nav">
+                <h1>Diversify</h1>
             </div>
             <div className="convertSection">
-                <h1>Convert To</h1>
-                <select className="dropdown">
-                    <option value="option1">Option 1</option>
-                    <option value="option2">Option 2</option>
-                    <option value="option3">Option 3</option>
-                </select>
+                <div className="convertFrom">
+                    <h1>Convert From</h1>
+                    <select className="dropdown">
+                        <option value="option1">Option 1</option>
+                        <option value="option2">Option 2</option>
+                        <option value="option3">Option 3</option>
+                    </select>
+                </div>
+                <h4>to</h4>
+                <div className="convertTo">
+                    <h1>Convert To</h1>
+                    <select className="dropdown">
+                        <option value="option1">Option 1</option>
+                        <option value="option2">Option 2</option>
+                        <option value="option3">Option 3</option>
+                    </select>
+                </div>
             </div>
         </div>
     );
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -8,9 +8,9 @@ const Main = () => {
                 <h1>Cross-Platform <span className='highlight'>Content</span> Diversification Tool</h1>
                 <h2>Bridge Platforms Seamlessly. Empower Your Presence Everywhere.</h2>
                 <div className="buttons">
-                    <button className='convertbutton'>Convert</button>
+                    <button className='convertbutton'>Convert &#128260;</button>
                     <h4>or</h4>
-                    <button className='secbutton'>Start Writting</button>
+                    <button className='secbutton'>Start Writting &#9999;&#65039;</button>
                 </div>
                 <div className="herofooter">
                     <h4>View <a href="/">Compatible Platforms &#8594;</a></h4>
